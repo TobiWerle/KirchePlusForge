6da56b1cd40efcdb4d6bb79eda3c522534b84015
TobiWerle <64752849+TobiWerle@users.noreply.github.com>
2023-03-30 20:11:05 +0200

    dev commit

--- src/main/java/UC/KirchePlus/Utils/RegistryHandler.java
+++ src/main/java/UC/KirchePlus/Utils/RegistryHandler.java
@@ -1,8 +1,16 @@
 package UC.KirchePlus.Utils;
 
+import java.io.IOException;
+
 public class RegistryHandler {
 
     public static void initRegistries() {
-        SoundsHandler.registerSounds();
+        try {
+            SoundsHandler.registerSounds();
+        } catch (IOException e) {
+            System.out.println("Fehler beim kopieren der Sounds");
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
