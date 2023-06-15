81cf2cc3c9b0447b2766b164468b38e00bd99494
TobiWerle <thefresh225@gmail.com>
2023-06-15 19:59:45 +0200

    fix

--- src/main/java/UC/KirchePlus/main/main.java
+++ src/main/java/UC/KirchePlus/main/main.java
@@ -16,7 +16,7 @@ import java.util.HashMap;
 public class main {
 	
     public static final String MODID = "kirche+";
-    public static final String VERSION = "3.1a";
+    public static final String VERSION = "3.1b";
     public static ArrayList<SpenderInfo> spender = new ArrayList<>();
     public static HashMap<Activity_User, Integer> totalActivity = new HashMap<>();
     public static ArrayList<FactionContract> FactionContracs = new ArrayList<>();
